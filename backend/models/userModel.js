f7c8bd444fcefd0b8bc9d7b328347786db3f348f
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-06-27 15:40:24 +0530

    mongoose Schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchmea = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchmea);
+export default User;
