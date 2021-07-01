949ea01f6ef3121625ce88a3b79abe8e5c969d75
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-07-01 11:27:36 +0530

    upto postman

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+// import products from "./data/products.js";
+
+import productRoutes from "./routes/productsRoutes.js";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -10,18 +15,22 @@ connectDB();
 
 const app = express();
 
+// For example purpose I have used this middleware
+// app.use((req, res, next) => {
+// console.log(req.originalUrl);
+//   console.log("hello");
+//   next();
+// });
+
 app.get("/", (req, res) => {
   res.send("My home page Api is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
