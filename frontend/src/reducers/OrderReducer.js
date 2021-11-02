ab1ef5186ac39b48cb8ca6b882fc27825e908b26
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-02 23:42:59 +0530

    dealing with hreact routes

---
+++ frontend/src/reducers/OrderReducer.js
@@ -0,0 +1,16 @@
+import {
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_FAIL,
+} from "../constants/OrderConstants";
+
+export const orderCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_CREATE_REQUEST:
+      return { loading: true };
+    case ORDER_CREATE_SUCCESS:
+      return { loading: true, success: true, order: action.payload };
+    case ORDER_CREATE_SUCCESS:
+      return { loading: false, error: action.payload };
+  }
+};
