147ff0b6766e419d48bda471b9a4032f1f330e60
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-02 23:11:47 +0530

    backend routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 import { addOrderItems } from "../controllers/OrderController.js";
 import { protectedroute } from "../middleware/authMiddleware.js";
 
+// protected is our middleware which is created by us
 router.route("/").post(protectedroute, addOrderItems);
 
 export default router;
+
+//  when we hit this route from action after order creating database in frontend we will be sent to specific page like orders/something
