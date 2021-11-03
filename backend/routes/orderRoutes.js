fcb2e69f967d3fe70cad7d20642a8b995336a907
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-03 23:13:48 +0530

    updated lines

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,7 +5,8 @@ import { protectedroute } from "../middleware/authMiddleware.js";
 
 // protected is our middleware which is created by us
 router.route("/").post(protectedroute, addOrderItems);
+
+//  when we hit this route from action after order creating database in frontend we will be sent to specific page like orders/something
 router.route("/:id").get(protectedroute, getOrderById); //if we are passing /:something better to keep it at bottom
 export default router;
 
-//  when we hit this route from action after order creating database in frontend we will be sent to specific page like orders/something
