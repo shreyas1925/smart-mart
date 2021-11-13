2122afbb46ee391747d0f86e8d1794ca6e948257
Shreyas <70105807+shreyas1925@users.noreply.github.com>
2021-11-13 23:18:07 +0530

    Update cartAction.js

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -24,6 +24,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
+
 export const removeFromCart = (id) => async (dispatch, getState) => {
   dispatch({
     type: CART_ITEM_REMOVE,
