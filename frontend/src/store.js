f98c9346fce2348320e963a8507e130e5ed6ccf6
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-19 19:30:19 +0530

    displaying all orders

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,6 +16,7 @@ import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
+  orderListMyReducer
 } from "./reducers/OrderReducers";
 const reducer = combineReducers({
   productList: productListReducers,
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderListMy:orderListMyReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
