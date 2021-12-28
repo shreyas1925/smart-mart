34c519238a9deb9bc6b25791d5147d98729d19b7
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-28 18:45:27 +0530

    working on redux

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,9 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_DETAILS_RESET,
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -76,6 +79,22 @@ export const userUpdateProfileReducer = (state = { user: {} }, action) => {
       return { loading: false, success: true, userInfo: action.payload };
 
     case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const userListReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true };
+
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+
+    case USER_LIST_FAIL:
       return { loading: false, error: action.payload };
 
     default:
