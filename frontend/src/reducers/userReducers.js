d8eb12eaf7db0b3af959603eb202f0fc74ea843e
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-01 19:14:28 +0530

    working on admin panel -> delete a specific user

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -17,6 +17,9 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -100,6 +103,22 @@ export const userListReducer = (state = { user: {} }, action) => {
 
     case USER_LIST_RESET:
       return { users: [] };
+
+    default:
+      return state;
+  }
+};
+
+export const userDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true };
+
+    case USER_DELETE_SUCCESS:
+      return { loading: false, users: action.payload };
+
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload };
 
     default:
       return state;
