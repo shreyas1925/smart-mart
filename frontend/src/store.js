708eec15f5019bd3ee8acda775e40719c633de7d
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-01 19:22:12 +0530

    user actions on delete user

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
   userListReducer,
+  userDeleteReducer,
 } from "./reducers/userReducers";
 import { userLoginReducer } from "./reducers/userReducers";
 import {
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  deleteUser: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
