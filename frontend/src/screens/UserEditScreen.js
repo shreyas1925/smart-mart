7ddf065475151c47b21d2ab21d911ad252eff36e
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-06 23:19:20 +0530

    done with user profiles

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -6,32 +6,41 @@ import Message from "../Components/Message";
 import Loader from "../Components/Loader";
 import { register } from "../actions/userAction"; //it is the action
 import FormComponent from "../Components/FormComponent";
-import { getuserDetails } from "../actions/userAction";
+import { getuserDetails, updateUser } from "../actions/userAction";
 import "./Login.css";
-
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 const UserEditScreen = ({ match, history }) => {
   const userID = match.params.id;
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [isAdmin, setIsAdmin] = useState(false);
-
   const dispatch = useDispatch();
-
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails; //getting from reducers
-
+  const updateUser = useSelector((state) => state.updateUser);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = updateUser; //getting from reducers
   useEffect(() => {
-    if (!user.name || user._id !== userID) {
-      dispatch(getuserDetails(userID));
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push("/admin/userlist");
     } else {
-      setName(user.name);
-      setEmail(user.email);
-      setIsAdmin(user.isAdmin);
+      if (!user.name || user._id !== userID) {
+        dispatch(getuserDetails(userID));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
     }
-  }, [dispatch, userID, user]);
+  }, [dispatch, userID, user, successUpdate, history]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateUser({ _id: userID, name, email, isAdmin }));
   };
 
   return (
@@ -42,6 +51,8 @@ const UserEditScreen = ({ match, history }) => {
 
       <FormComponent>
         <h1>Edit User {userID}</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
